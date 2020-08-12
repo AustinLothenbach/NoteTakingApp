c5190aca8815b2675491685c5af2525b1c4dcd1b
Austin Lothenbach <austin.lothenbach@smail.rasmussen.edu>
2020-08-11 21:31:05 -0500

    Updates

---
+++ precache-manifest.6fd7e81cbc6ff75c8250cc78b49d5cd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9cc49c0644081e909b87a3e8c2f647c",
+    "url": "/NoteTakingApp/index.html"
+  },
+  {
+    "revision": "776bc93e86670bd765ea",
+    "url": "/NoteTakingApp/static/css/main.e6e4ea98.chunk.css"
+  },
+  {
+    "revision": "aa3919c409104e3d7107",
+    "url": "/NoteTakingApp/static/js/2.91b9cb10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NoteTakingApp/static/js/2.91b9cb10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776bc93e86670bd765ea",
+    "url": "/NoteTakingApp/static/js/main.fe19fc7d.chunk.js"
+  },
+  {
+    "revision": "28b16da756681b2b46ca",
+    "url": "/NoteTakingApp/static/js/runtime-main.d01056b0.js"
+  }
+]);
